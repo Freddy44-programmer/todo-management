0a35415fc67cd53f071bc79ae1a6aadd80a2b085
Thabang <Thabang@DESKTOP-867BMT0>
2024-07-22 18:52:21 +0200

    Initial commit

---
+++ src/main/java/Codewithfreddy/todo_management/repository/TodoRepository.java
@@ -0,0 +1,7 @@
+package Codewithfreddy.todo_management.repository;
+
+import Codewithfreddy.todo_management.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
---
+++ src/main/java/Codewithfreddy/todo_management/service/TodoService.java
@@ -0,0 +1,22 @@
+package Codewithfreddy.todo_management.service;
+
+import Codewithfreddy.todo_management.dto.TodoDto;
+
+import java.util.List;
+
+public interface TodoService {
+
+    TodoDto addTodo(TodoDto todoDto);
+
+    TodoDto getTodo(Long id);
+
+    List<TodoDto> getAllTodos();
+
+    TodoDto updateTodo(TodoDto todoDto, Long id);
+
+    void deleteTodo(Long id);
+
+    TodoDto completeTodo(Long id);
+
+    TodoDto inCompleteTodo(Long id);
+}
